42f20e8d02a1518164a91607a832b0ac304ff06f
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2023-12-01 15:41:18 +0100

    adapt to derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fajita.mk
+    $(LOCAL_DIR)/derp_fajita.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fajita-user \
-    lineage_fajita-userdebug \
-    lineage_fajita-eng
+    derp_fajita-user \
+    derp_fajita-userdebug \
+    derp_fajita-eng
